0dcbc621d6f84f3c43567b3838b0783446709d8b
Darksider379 <mukherjeechandra379@gmail.com>
2021-09-05 23:30:52 +0530

    commit

---
+++ Modules.py
@@ -0,0 +1,12 @@
+import os
+import time
+import pandas
+
+while True:
+    if os.path.exists(r'C:\Users\CHANDRABRATA\Desktop\Python Basic\UdemyResources\temps_today.csv'):
+        pd=pandas.read_csv(r'C:\Users\CHANDRABRATA\Desktop\Python Basic\UdemyResources\temps_today.csv')
+        print(pd.mean()['st1'])
+    
+    else:
+        print('File not found')
+    time.sleep(5)
